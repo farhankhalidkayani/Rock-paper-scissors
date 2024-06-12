3f5c184c56615628eb45aa8d24796a48e595c0f5
Farhan Khalid <rj.farhan4232@gmail.com>
2024-06-12 17:22:01 +0500

    Improved the code by telling user their and computers choice and merged multiple cases

--- rps.js
+++ rps.js
@@ -4,27 +4,18 @@ while(true){
     if(choice==1){
 let userinput=parseInt(prompt("Please enter your choice: \n(1) for Rock\n(2) for Paper\n(3) for Scissors"));
 let computerchoice=parseInt((Math.random()*3)+1);
+const choices=["Rock","Paper","Scissors"]
 if(userinput==computerchoice){
-    console.log(`Tie!!! Your Choice was ${userinput} and computers choice was ${computerchoice}`);
+    console.log(`Tie!!! Your Choice was ${choices[userinput-1]} and computers choice was ${choices[computerchoice-1]}`);
 }
-else if(userinput==1 && computerchoice==2){
-    console.log(`You Lose!!! Your Choice was ${userinput} and computers choice was ${computerchoice}`);
+else if((userinput==1 && computerchoice==2)||(userinput==2 && computerchoice==3)||(userinput==3 && computerchoice==1)){
+    console.log(`You Lose!!! Your Choice was ${choices[userinput-1]} and computers choice was ${choices[computerchoice-1]}`);
 }
-else if(userinput==2 && computerchoice==3){
-    console.log(`You Lose!!! Your Choice was ${userinput} and computers choice was ${computerchoice}`);
+
+else if((userinput==1 && computerchoice==3)||(userinput==2 && computerchoice==1)||(userinput==3 && computerchoice==2)){
+    console.log(`You Win!!! Your Choice was ${choices[userinput-1]} and computers choice was ${choices[computerchoice-1]}`);
 }
-else if(userinput==3 && computerchoice==1){
-    console.log(`You Lose!!! Your Choice was ${userinput} and computers choice was ${computerchoice}`);
-}
-else if(userinput==1 && computerchoice==3){
-    console.log(`You Win!!! Your Choice was ${userinput} and computers choice was ${computerchoice}`);
-}
-else if(userinput==2 && computerchoice==1){
-    console.log(`You Win!!! Your Choice was ${userinput} and computers choice was ${computerchoice}`);
-}
-else if(userinput==3 && computerchoice==2){
-    console.log(`You Win!!! Your Choice was ${userinput} and computers choice was ${computerchoice}`);
-}
+
 else{
     console.log("Invalid Input");
 }}
