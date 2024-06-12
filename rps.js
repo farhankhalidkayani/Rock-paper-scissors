cf1964ce736248181cafcbcd7a9b498793451672
Farhan Khalid <rj.farhan4232@gmail.com>
2024-06-12 18:07:18 +0500

    Made Functions for all the processes and added score keeping system

--- rps.js
+++ rps.js
@@ -1,26 +1,37 @@
+let humanScore=0;
+let computerScore=0;
+const getComputerChoice=()=>(parseInt((Math.random()*3)+1));
+const getHumanChoice=()=>(parseInt(prompt("Please enter your choice: \n(1) for Rock\n(2) for Paper\n(3) for Scissors")));
+const playRound=(userInput,computerChoice)=>{const choices=["Rock","Paper","Scissors"]
+    if(userInput==computerChoice){
+        console.log(`Tie!!! Your Choice was ${choices[userInput-1]} and computers choice was ${choices[computerChoice-1]}`);
+    }
+    else if((userInput==1 && computerChoice==2)||(userInput==2 && computerChoice==3)||(userInput==3 && computerChoice==1)){
+        console.log(`You Lose!!! Your Choice was ${choices[userInput-1]} and computers choice was ${choices[computerChoice-1]}`);
+        computerScore++;
+    }
+    
+    else if((userInput==1 && computerChoice==3)||(userInput==2 && computerChoice==1)||(userInput==3 && computerChoice==2)){
+        console.log(`You Win!!! Your Choice was ${choices[userInput-1]} and computers choice was ${choices[computerChoice-1]}`);
+        humanScore++;
+    }
+    
+    else{
+        console.log("Invalid Input");
+    }};
 
 while(true){
-    let choice=parseInt(prompt("Please select from Following options\n(1)To play Rock Paper Scissors\n(2)To Quit"));
+    let choice=parseInt(prompt("Please select from Following options\n(1)To play Rock Paper Scissors\n(2)To View Scores\n(3)To Quit"));
     if(choice==1){
-let userinput=parseInt(prompt("Please enter your choice: \n(1) for Rock\n(2) for Paper\n(3) for Scissors"));
-let computerchoice=parseInt((Math.random()*3)+1);
-const choices=["Rock","Paper","Scissors"]
-if(userinput==computerchoice){
-    console.log(`Tie!!! Your Choice was ${choices[userinput-1]} and computers choice was ${choices[computerchoice-1]}`);
+let userInput=getHumanChoice();
+let computerChoice=getComputerChoice();
+playRound(userInput,computerChoice);
 }
-else if((userinput==1 && computerchoice==2)||(userinput==2 && computerchoice==3)||(userinput==3 && computerchoice==1)){
-    console.log(`You Lose!!! Your Choice was ${choices[userinput-1]} and computers choice was ${choices[computerchoice-1]}`);
-}
-
-else if((userinput==1 && computerchoice==3)||(userinput==2 && computerchoice==1)||(userinput==3 && computerchoice==2)){
-    console.log(`You Win!!! Your Choice was ${choices[userinput-1]} and computers choice was ${choices[computerchoice-1]}`);
-}
-
-else{
-    console.log("Invalid Input");
-}}
 else if(choice==2){
-break;
+console.log(`Your Score is ${humanScore} and Computers Score is ${computerScore}`)
+}
+else if(choice==3){
+    break;
 }
 else{
     console.log("Invalid Choice");
